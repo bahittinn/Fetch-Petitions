2d3c490875ac70e8cfa113768dca3bd74b712a83
bahittinn <bahittin84@gmail.com>
2023-08-04 14:13:35 +0300

    decoded data

--- Fetch-Petition/ViewController.swift
+++ Fetch-Petition/ViewController.swift
@@ -16,17 +16,28 @@ class ViewController: UITableViewController {
         let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
         
         if let url = URL(string: urlString) {
-            if let data = try? Data(contentsOf: url) {
-                
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) {( data, response, error) in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                if let data = data {
+                    // Veriyi işleme
+                    // JSON verisini çözümle
+                    self.parse(json: data)
+                }
             }
+            task.resume()
         }
     }
     func parse(json: Data) {
         let decoder = JSONDecoder()
-        
-        if let jsonPetitions = try?  decoder.decode(Petitions.self, from: json) {
-            petitions = jsonPetitions.result
-            tableView.reloadData()
+        let jsonPetitions = try? decoder.decode(Petitions.self, from: json)
+        self.petitions = jsonPetitions!.results
+        print("deneme: \(self.petitions[0])")
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
         }
     }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -34,8 +45,9 @@ class ViewController: UITableViewController {
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell",for: indexPath)
-        cell.textLabel?.text = "title"
-        cell.detailTextLabel?.text = "detail"
+        let petition = petitions[indexPath.row]
+        cell.textLabel?.text = petition.title
+        cell.detailTextLabel?.text = petition.body
         return cell
     }
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
